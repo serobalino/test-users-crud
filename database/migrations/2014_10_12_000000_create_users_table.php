8f68d5d4c0e3853ee4af92c57056aac15398d794
Sebastian <odm_sd@hotmail.com>
2022-03-29 15:22:30 -0500

    new: nuevos campos segun ejercicio

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('isAdmin')->default(false);
+            $table->timestamp('lastLogin')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
